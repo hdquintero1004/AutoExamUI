bdbee9846fcca96e86b7d968a2cbe0991fc1fa7d
Hector David Quintero <hdquintero1004@gmail.com>
2018-02-22 22:14:20 -0500

    Working in Home Page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     if not user_signed_in?
       redirect_to(user_session_path)
     else
-
+      @signatures = Teacher.select(:signature_id).where(user_id: current_user.id)
     end
   end
 end
