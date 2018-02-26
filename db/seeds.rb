0472ef1e9cadb604fa22afdcc5d6636e56b7f2b2
Hector David Quintero <hdquintero1004@gmail.com>
2018-02-25 21:16:55 -0500

    Changes in Home Page. Adding options for admin users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Signature.create([{:name => "Arquitectura de Computadoras"}, {:name => "Programacion de Maquinas"}])
 Role.create([ {name: "Admin"}, {name: "Profesor"}])
-User.create(:email => "admin@matcom.uh.cu", :password => "admin123", :password_confirmation => "admin123", :role_id => 1)
+User.create([{:email => "admin@matcom.uh.cu", :password => "admin123", :password_confirmation => "admin123", :role_id => 1}, {:email => "hd@matcom.uh.cu", :password => "123456", :password_confirmation => "123456", :role_id => 2 }])
