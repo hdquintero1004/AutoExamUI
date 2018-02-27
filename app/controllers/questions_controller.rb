0e1ffc70cea51fb7355b6646668ed9910675baca
Hector David Quintero <hdquintero1004@gmail.com>
2018-02-26 22:38:40 -0500

    Trying to create select list with checkboxes in questions views.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question.where(:signature_id => params[:signature_id])
+    @questions = Question.All
   end
 
   # GET /questions/1
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,10 @@
 class Question < ApplicationRecord
   belongs_to :signature
   has_many :options, :dependent => :destroy
+
+  validate :labels_in_signature?
+
+  def labels_in_signature?
+    signature_labels = Signature.find(signature_id).labels
+  end
 end
